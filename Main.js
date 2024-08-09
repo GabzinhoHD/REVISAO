8b05409960b58dc34523d9f15be16d96ac3fa46e
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2024-08-09 10:38:12 -0300

    Iniciando o Javascript

---
+++ Main.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector('.caixa-principal')
+const caixaPerguntas = document.querySelector('.caixa-perguntas')
+const caixaAlternativas = document.querySelector('.caixa-alternativas')
+const caixaResultado = document.querySelector('.caixa-resultado')
+const textoResultado = document.querySelector('.texto-resultado')
+
+const perguntas = [
+    {
+        enunciado: "Texto 1",
+        Alternativas:[
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Texto 2",
+        Alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]
